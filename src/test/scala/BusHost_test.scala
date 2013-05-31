89ac5a6afaaea85cd0e647919b7a202baf2113b4
nick <nick@ntcoding.co.uk>
2013-05-31 07:26:29 +0100

    outline of a first unit test for the dbus host

--- src/test/scala/BusHost_test.scala
+++ src/test/scala/BusHost_test.scala
@@ -1,5 +1,24 @@
 
 
+import org.mockito.Mockito._
+import org.mockito.stubbing._
+import org.junit._
+import junit.framework.Assert._
+
 class BusHost_test {
 
+    @Before
+    def when_starting_up_the_bus_host {
+
+    }
+
+    @Test
+    def command_inbox_interface_is_exported_onto_dbus_with_reference_to_a_command_handler_actor {
+        assertFalse(true)
+    }
+
+    @Test
+    def command_sender_interface_is_exported_onto_dbus_with_reference_to_a_command_sender_actor {
+        assertFalse(true)
+    }
 }
