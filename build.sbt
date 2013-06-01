8ac2d172c34eba428b0510315166b33d5201f419
nick <nick@ntcoding.co.uk>
2013-06-01 14:49:49 +0100

    moved to mockito - scala mock just doesn't work

--- build.sbt
+++ build.sbt
@@ -8,4 +8,7 @@ libraryDependencies += "junit" % "junit" % "4.10" % "test"
 
 libraryDependencies += "com.novocode" % "junit-interface" % "0.8" % "test"
 
-libraryDependencies += "org.scalamock" %% "scalamock-scalatest-support" % "3.0.1" % "test"
+libraryDependencies += "org.scalatest" % "scalatest_2.10" % "2.0.M6-SNAP17"
+
+libraryDependencies += "org.mockito" % "mockito-all" % "1.9.5"
+
--- src/test/scala/BusHost_test.scala
+++ src/test/scala/BusHost_test.scala
@@ -1,15 +1,14 @@
+package test
 
-
-
-import akkesb.dbus.{TestableDBusConnection, DBusMessageSender, ActorDelegatingMessageHandler}
+import akkesb.dbus._
 import akkesb.host.BusHost
 import org.freedesktop.dbus.{DBusInterface, DBusAsyncReply, DBusConnection}
-import org.scalamock.scalatest.MockFactory
-import org.scalatest.FreeSpec
+import org.scalatest.{OneInstancePerTest, BeforeAndAfter, BeforeAndAfterAll, FreeSpec}
 import scala.language.postfixOps
+import org.scalatest.mock.MockitoSugar
+import org.mockito.Mockito._
 
-
-class Bus_host_startup_spec extends FreeSpec with MockFactory  {
+class Bus_host_startup_spec extends FreeSpec with OneInstancePerTest with MockitoSugar  {
 
     val hostName = "funnyhostname"
     val port = "2892"
@@ -19,14 +18,12 @@ class Bus_host_startup_spec extends FreeSpec with MockFactory  {
     val sender = new DBusMessageSender()
     val connection = mock[TestableDBusConnection]
 
+    "When starting up the bus host" - {
 
-    "When starting up the bus akkesb.host" - {
-
-        BusHost(hostName, port, application, service, handler, sender, connection);
-
+        BusHost(hostName, port, application, service, handler, sender, connection)
 
         "it registers as a service on akkesb using the supplied application and service name" in {
-            connection.requestBusName _ verify f"akkesb.$application.$service"
+            verify(connection) requestBusName f"akkesb.$application.$service"
         }
 
         "it exports the supplied command handler on akkesb in the messages/incoming path" in {
@@ -36,8 +33,9 @@ class Bus_host_startup_spec extends FreeSpec with MockFactory  {
         "it exports the supplied command sender on akkesb in the messages/outgoing path" in {
 
         }
-
     }
+
+
 }
 
 
