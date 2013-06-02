0579155ee12e64648f73f98fce23a75197ea9ce1
nick <nick@ntcoding.co.uk>
2013-06-02 08:24:50 +0100

    stubbed message send actor so end to end tests fail for right reason

--- src/main/scala/akkesb/host/MessageSendActor.scala
+++ src/main/scala/akkesb/host/MessageSendActor.scala
@@ -4,6 +4,8 @@ import akka.actor.Actor
 
 class MessageSendActor extends Actor {
 
-    def receive: MessageSendActor#Receive = throw new Exception("Receive not implemented")
+    def receive: MessageSendActor#Receive =  {
+        case _ => println("message send actor not implemented yet")
+    }
 
 }
