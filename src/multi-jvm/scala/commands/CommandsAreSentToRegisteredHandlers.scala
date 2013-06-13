fe0059283b63d546b5883f09b3d7f2bc8f2a8fb1
nick <nick@crunchbang.(none)>
2013-06-13 21:10:35 +0100

    converted multi-jvm tests back into applications because the tests weren't working properly with DBus

--- src/multi-jvm/scala/commands/CommandsAreSentToRegisteredHandlers.scala
+++ src/multi-jvm/scala/commands/CommandsAreSentToRegisteredHandlers.scala
@@ -17,71 +17,77 @@ import org.scalatest.{FreeSpec, FreeSpecLike}
  */
 
 
-class CommandsAreSentToRegisteredHandlersMultiJvmMarketingServiceHost extends FreeSpec {
+object CommandsAreSentToRegisteredHandlersMultiJvmMarketingServiceHost extends FreeSpec {
 
-    "startup a marketing service that registers itself as a sender of update_price commands " - {
-        val connection = new AkkesbDBusConnection(DBusConnection.getConnection(DBusConnection.SESSION))
-        val actorSystem = ActorSystem.create("akkesb")
-        // TODO hostname and port should be passed in as args - come back to this later
-        val host = BusHost("127.0.0.1", "3051","commands_are_sent_test", "marketing_service",
-            new ActorDelegatingMessageHandler(), new DBusMessageSender(), connection, actorSystem)
-        // TODO - if these are not set at startup - they will need to be sent via akkesb instead
-        host willSendCommands List(("update_price"))
-        host joinCluster
-    }
-}
-
-class CommandsAreSentToRegisteredHandlersMultiJvmCatalogueServiceHost extends FreeSpec {
-
-    "startup a catalogue service that sends stop_taking_payments_for_product commands and handles update_price commands" - {
-        var receivedMessages : List[(String, Array[String], Array[String])] = List()
-        val tmh = new TestMessageHandler((name, keys, values) => receivedMessages = receivedMessages :+ (name, keys, values))
-
-        val connection = new AkkesbDBusConnection(DBusConnection.getConnection(DBusConnection.SESSION))
-        val actorSystem = ActorSystem.create("akkesb")
-        // TODO - first 3 params can group as a data structure
-        val host = BusHost("127.0.0.1", "3052", "commands_are_sent_test", "catalogue_service", tmh, new DBusMessageSender(),
-            connection, actorSystem)
-
-        host willSendCommands List("stop_taking_payments_for_product")
-        host willHandleCommands List("update_price")
-        host joinCluster
-
-        "the catalogue service will have received the update_price command sent from the client" in {
-            Thread.sleep(1000) // wait for messages to be sent from the client library simulation
-            Assert.assertTrue("catalogue service did not receive the update price command", receivedMessages contains ("update_price", List(("productId", 1), ("price", 50))))
+    def main(args: Array[String]) {
+        "startup a marketing service that registers itself as a sender of update_price commands " - {
+            val connection = new AkkesbDBusConnection(DBusConnection.getConnection(DBusConnection.SESSION))
+            val actorSystem = ActorSystem.create("akkesb")
+            // TODO hostname and port should be passed in as args - come back to this later
+            val host = BusHost("127.0.0.1", "3051","commands_are_sent_test", "marketing_service",
+                new ActorDelegatingMessageHandler(), new DBusMessageSender(), connection, actorSystem)
+            // TODO - if these are not set at startup - they will need to be sent via akkesb instead
+            host willSendCommands List(("update_price"))
+            host joinCluster
         }
     }
 }
 
-class CommandsAreSentToRegisteredHandlersMultiJvmPaymentsServiceHost extends FreeSpec {
+object CommandsAreSentToRegisteredHandlersMultiJvmCatalogueServiceHost extends FreeSpec {
 
-    "Startup a payments service that handles stop_taking_payments_for_product commands" - {
-        var receivedMessages : List[(String, Array[String], Array[String])] = List()
-        val tmh = new TestMessageHandler((name, keys, values) => receivedMessages = receivedMessages :+ (name, keys, values))
+    def main(args: Array[String]) {
+        "startup a catalogue service that sends stop_taking_payments_for_product commands and handles update_price commands" - {
+            var receivedMessages : List[(String, Array[String], Array[String])] = List()
+            val tmh = new TestMessageHandler((name, keys, values) => receivedMessages = receivedMessages :+ (name, keys, values))
 
-        val connection = new AkkesbDBusConnection(DBusConnection.getConnection(DBusConnection.SESSION))
-        val actorSystem = ActorSystem.create("akkesb")
+            val connection = new AkkesbDBusConnection(DBusConnection.getConnection(DBusConnection.SESSION))
+            val actorSystem = ActorSystem.create("akkesb")
+            // TODO - first 3 params can group as a data structure
+            val host = BusHost("127.0.0.1", "3052", "commands_are_sent_test", "catalogue_service", tmh, new DBusMessageSender(),
+                connection, actorSystem)
 
-        val host = BusHost("127.0.0.1", "3053", "commands_are_sent_test", "payments_service", tmh,
-            new DBusMessageSender(), connection, actorSystem)
+            host willSendCommands List("stop_taking_payments_for_product")
+            host willHandleCommands List("update_price")
+            host joinCluster
 
-        host willHandleCommands List("stop_taking_payments_for_product")
-        host joinCluster
-
-        "the payments service will have recieved the stop_taking_paments_for_product command that was sent from the client" in {
-            Thread.sleep(1000) // wait for messages to be sent from the client library simulation
-            Assert.assertTrue("payments service did not receive stop taking payments command", receivedMessages contains ("stop_taking_payments_for_product", List(("productId", 1))))
+            "the catalogue service will have received the update_price command sent from the client" in {
+                Thread.sleep(1000) // wait for messages to be sent from the client library simulation
+                Assert.assertTrue("catalogue service did not receive the update price command", receivedMessages contains ("update_price", List(("productId", 1), ("price", 50))))
+            }
         }
     }
 }
 
-class CommandsAreSentToRegisteredHandlersMultiJvmClientLibrarySimulation extends FreeSpec {
+object CommandsAreSentToRegisteredHandlersMultiJvmPaymentsServiceHost extends FreeSpec {
+
+    def main(args: Array[String]) {
+        "Startup a payments service that handles stop_taking_payments_for_product commands" - {
+            var receivedMessages : List[(String, Array[String], Array[String])] = List()
+            val tmh = new TestMessageHandler((name, keys, values) => receivedMessages = receivedMessages :+ (name, keys, values))
+
+            val connection = new AkkesbDBusConnection(DBusConnection.getConnection(DBusConnection.SESSION))
+            val actorSystem = ActorSystem.create("akkesb")
+
+            val host = BusHost("127.0.0.1", "3053", "commands_are_sent_test", "payments_service", tmh,
+                new DBusMessageSender(), connection, actorSystem)
+
+            host willHandleCommands List("stop_taking_payments_for_product")
+            host joinCluster
+
+            "the payments service will have recieved the stop_taking_paments_for_product command that was sent from the client" in {
+                Thread.sleep(1000) // wait for messages to be sent from the client library simulation
+                Assert.assertTrue("payments service did not receive stop taking payments command", receivedMessages contains ("stop_taking_payments_for_product", List(("productId", 1))))
+            }
+        }
+    }
+}
+
+object CommandsAreSentToRegisteredHandlersMultiJvmClientLibrarySimulation extends FreeSpec {
     /*
         Messages are sent from a separate JVM replicating how a real system work - client
         libraries will send messages via akkesb from separate processes
      */
-    "Simulate a client library that sends an update_price and stop_taking_payments_for_product command" - {
+    def main(args: Array[String]) {
         Thread.sleep(2000) // wait for services to start up - might be a better way than sleeping
 
         Command(("update_price", List(("productId", 1), ("price", 50))))
