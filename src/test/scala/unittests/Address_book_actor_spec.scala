c68c09ff4e00d89525750e8b6c895b9fa1089e88
nick <nick@ntcoding.co.uk>
2013-06-15 12:48:48 +0100

    address book actor spec now sets up and asserts correctly

--- src/test/scala/unittests/Address_book_actor_spec.scala
+++ src/test/scala/unittests/Address_book_actor_spec.scala
@@ -1,30 +1,41 @@
 package unittests
 
 import akka.testkit.{TestActorRef, TestKit}
-import akka.actor.{Props, ActorSystem}
+import akka.actor.{Actor, Props, ActorSystem}
 import org.scalatest.FreeSpecLike
-import akkesb.host.{ReferenceToAddress, WhatIsTheAddressFor, AddAddress, AddressBookActor}
+import akkesb.host._
 import scala.concurrent.duration.Duration
 import org.scalatest.MustMatchers
+import akkesb.host.ReferenceToAddress
+import akkesb.host.AddAddress
+import akkesb.host.WhatIsTheAddressFor
+import com.typesafe.config.ConfigFactory
 
-class Address_book_actor_spec extends TestKit(ActorSystem("TestActorSystem989")) with FreeSpecLike with MustMatchers with StopSystemAfterAll{
+class Address_book_actor_spec extends TestKit(ActorSystem("TestActorSystem989", ConfigFactory.load(ConfigFactory.parseString("""akka.actor { provider = "akka.remote.RemoteActorRefProvider" }"""))))
+                                with FreeSpecLike with MustMatchers with StopSystemAfterAll{
 
     val keys = Array("1", "2", "3")
     val values = Array("4", "5", "6")
 
-    "When telling the address book to store an address" - {
+    "When telling the address book to store an address for an actor that already exists" - {
 
-        val addressBook = TestActorRef(Props(()  => new AddressBookActor("testApplication")))
-        addressBook ! AddAddress("7digital", "456.289.33.33", "8876")
+        val rs = new RemoteActorSystemCreator().create("7dizzo", "127.0.0.1", "6612")
+        val act = rs.actorOf(new Props(() => new Actor{ def receive = {case _ => println("hello")}}), "7digital")
+
+        val addressBook = TestActorRef(Props(() => new AddressBookActor("7dizzo")))
+        addressBook ! AddAddress("7digital", "127.0.0.1", "6612")
         addressBook.tell(WhatIsTheAddressFor("7digital", ("command", keys, values)), testActor)
+
 
         "Future requests for that address return an actor ref pointing to the remote actor with the original command data" in {
             receiveOne(Duration(200, "ms")) match {
                 case ref: ReferenceToAddress => {
-                    ref.reference.path.address must equal("akka://testApplicatione@456.289.33.33:8876/user/7digital")
+                    ref.reference.path.toString must equal("akka://7dizzo@127.0.0.1:6612/user/7digital")
                     ref.command must equal(("command", keys, values))
                 }
             }
         }
+
+        rs.shutdown()
     }
 }
