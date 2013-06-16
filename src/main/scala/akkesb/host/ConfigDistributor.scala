30ba9c3eb8d9e9fd20ab7c4722aa89205723bed9
nick <nick@ntcoding.co.uk>
2013-06-16 08:33:56 +0100

    config distributor goes to address book and forwards builds mesages that it tells endpoint to send

--- src/main/scala/akkesb/host/ConfigDistributor.scala
+++ src/main/scala/akkesb/host/ConfigDistributor.scala
@@ -5,6 +5,11 @@ import akka.actor.{ActorRef, Actor}
 class ConfigDistributor(addressBook: ActorRef, serviceEndpoint: ActorRef) extends Actor {
 
     def receive = {
-        case _ => println("Config distributor net set up to handle messages")
+        case DistributeCommandOwnership(owner, commands) => addressBook ! GiveMeAReferenceToAllServiceAddresses((owner, commands))
+        case AllServices(services, (owner: String, commands: List[String])) => serviceEndpoint ! SendRemoteMessages(messagesFor(services, owner, commands))
+    }
+
+    def messagesFor(services: List[ActorRef], commandOwner: String, commandNames: List[String]): List[(ActorRef, AnyRef)] = {
+        services map(s => (s, CommandHandlerRegistrations(commandOwner, commandNames)))
     }
 }
