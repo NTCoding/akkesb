2138ae97df5e38d5903c4b22d4ed4a48cf2c6064
nick <nick@ntcoding.co.uk>
2013-06-16 13:53:47 +0100

    added some extra logging in service endpoint

--- src/main/scala/akkesb/host/ServiceEndpoint.scala
+++ src/main/scala/akkesb/host/ServiceEndpoint.scala
@@ -11,11 +11,20 @@ class ServiceEndpoint(registrar: ActorRef, handler: MessageHandler) extends Acto
     def receive = {
         case SendRemoteMessages(toSend) => toSend foreach(x => sendRemoteMessage(x))
 
-        case CommandHandlerRegistrations(service, commands) => registrar ! RegisterMultipleCommandsHandler(commands, service)
+        case CommandHandlerRegistrations(service, commands) => {
+            log.info(s"Received command handler registrations: ${commands.mkString(",")} from $service")
+            registrar ! RegisterMultipleCommandsHandler(commands, service)
+        }
 
-        case SendCommandToService(service, command) => service ! ProcessCommand(command)
+        case SendCommandToService(service, command) => {
+            log.info(s"About to send command: ${command._1} to service: ${service.path}")
+            service ! ProcessCommand(command)
+        }
 
-        case ProcessCommand(command) => handler handle(command._1, command._2, command._3)
+        case ProcessCommand(command) => {
+            log.info(s"About to process command: ${command._1}")
+            handler handle(command._1, command._2, command._3)
+        }
 
         case any => throw new UnsupportedOperationException(s"Service endpoint not configured to handle: $any")
     }
