6426f037fdc30bdbff1f60a27200afc984733397
Nick <nick@ntcoding.co.uk>
2013-06-18 10:58:31 +0100

    template for a sample application

---
+++ samples/no_more_recruiters/ruby_web_admin_interface/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RubyWebAdminInterface::Application.config.secret_token = '46b1c9607e30b07fcc7398f2354d7589b1be286a84e91a64af85a70b0889b7dd5c59411b6f718b3807082102b0910d8cb5adf2d5c226e72dced6fa018f9373ea'
