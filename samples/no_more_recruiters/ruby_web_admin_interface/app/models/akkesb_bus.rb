28753603749c150aea99d53b28b93afbd8b49d5f
Nick <nick@ntcoding.co.uk>
2013-06-19 09:53:24 +0100

    ruby bus client tries to send message to host

--- samples/no_more_recruiters/ruby_web_admin_interface/app/models/akkesb_bus.rb
+++ samples/no_more_recruiters/ruby_web_admin_interface/app/models/akkesb_bus.rb
@@ -3,14 +3,18 @@ require 'dbus'
 
 class AkkesbBus
 	include Singleton
+	@bus
 
 	def initialize
-		bus = DBus::SessionBus.instance
-		bus.request_service("akkesb.NoMoreRecruiters.RubyWebApp.Client")
+		@bus = DBus::SessionBus.instance
+		@bus.request_service("akkesb.NoMoreRecruiters.RubyWebApp.Client")
 	end
 
-	def send(to, keys, values)
-		puts "yay" 
+	def send(command, keys, values)
+		host = @bus.service("akkesb.NoMoreRecruiters.RubyWebApp.Host").object("/messages/outgoing")
+		host.introspect
+		host.deault_iface = "akkesb.dbus.MessageSender"
+		host.send(command, keys, values)
 	end
 
 end
