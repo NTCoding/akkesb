447d7ded07913c16579975de9ae6458bf5374cd0
nick <nick@ntcoding.co.uk>
2013-06-23 19:37:08 +0100

    update rakefile and startup script - didn't work before syntax was wrong

--- akkesb_startup.sh
+++ akkesb_startup.sh
@@ -1,5 +1,6 @@
 #! /bin/sh
 
 this_dir=$(dirname $0)
-path_to_start_script=`echo $this_dir/bin/start`
-`$path_to_start_script akkesb.host.AkkesbStarter`
+path_to_start_script=`echo ${this_dir}/bin/start`
+`echo "about to run: $path_to_start_script"`
+$path_to_start_script akkesb.host.AkkesbStarter
