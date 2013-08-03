515384b99727e9393fe031a364f01f536af79002
nick <nick@ntcoding.co.uk>
2013-08-03 11:05:40 +0100

    first spray endpoints up and running with canned responses (jobs catalogue service)

--- samples/no_more_recruiters/JobsCatalogueService/src/main/scala/Boot.scala
+++ samples/no_more_recruiters/JobsCatalogueService/src/main/scala/Boot.scala
@@ -1,30 +1,82 @@
-import akka.actor.{ActorRefFactory, Actor, Props, ActorSystem}
+import akka.actor._
 import akka.io.IO
+import akka.util.Timeout
+import java.util.concurrent.TimeUnit
+import scala.concurrent.Future
+import scala.Some
+import scala.util.parsing.json.JSON
 import spray.can.Http
-import spray.routing
-import spray.routing.HttpService
+import akka.pattern.ask
+import spray.http._
+import spray.http.HttpHeaders.RawHeader
+import spray.http.HttpMethods._
+
 
 object Boot extends App {
 
     implicit val system = ActorSystem.create("JobsCatalogueService")
 
-    val service = system.actorOf(Props[SearchServiceActor])
+    val searcher = system.actorOf(Props[Searcher])
+    val service = system.actorOf(Props(classOf[SearchService], searcher))
 
     IO(Http) ! Http.Bind(service, "localhost", 8081)
 }
 
-class SearchServiceActor extends Actor with HttpService {
 
-    def routes = {
-        path("") {
-            get {
-                complete("hello")
+class SearchService (searcher: ActorRef) extends Actor {
+
+    implicit val timeout =  Timeout(20, TimeUnit.SECONDS)
+    implicit val executionContext = context.dispatcher
+
+    def receive = {
+
+        case _: Http.Connected => sender ! Http.Register(self)
+
+        case HttpRequest(GET, Uri.Path("/"), _, _, _) => {
+            sender ! HttpResponse(entity = "Try jobs/q={searchTerm}")
+        }
+
+        case HttpRequest(GET, uri,  _, _, _) => {
+            val endpoint = sender // close over the sender now, not when the future completes
+            handleGetRequestFor(uri) map {response => endpoint ! response }
+        }
+    }
+
+    def handleGetRequestFor(uri: Uri) = {
+        uri.path match {
+            case Uri.Path("/jobs") => handleJobsRequest(uri.query.get("q"))
+            case Uri.Path(path) => {
+                println(s"No handler for path: $path")
+                Future {HttpResponse(400, s"No handler for path: $path")}
             }
         }
     }
 
-    def receive = runRoute(routes)
+    def handleJobsRequest(q: Option[String]) = {
+        q match {
+            case None => Future{ HttpResponse(400, "Missing value for \"q\" parameter") }
+            case Some(searchTerm) => {
+                    (searcher ?  SearchFor(searchTerm)).mapTo[String] map {result =>
+                        println(s"Returning 200 response with entity: $result")
+                        HttpResponse(200, entity = result, headers = List(RawHeader("Content-Type", "application/json")))
+                    }
+            }
+        }
+    }
+}
 
-    implicit def actorRefFactory = context
+
+case class SearchFor(searchTerm: String)
+
+
+class Searcher extends Actor {
+
+    def receive = {
+        case SearchFor(searchTerm) => {
+            println(s"Searcher received search request for: $searchTerm")
+            sender ! """{ "result"="yippee" }"""
+        }
+    }
+
 }
 
